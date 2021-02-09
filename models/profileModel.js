d88bc12aec21118641ed86dc822b17aca976c90c
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-02-09 17:03:35 -0500

    working delete and edit

--- models/profileModel.js
+++ models/profileModel.js
@@ -19,13 +19,39 @@ class PROFILEModel {
     }
 
     static async getAllReviews(user_id) {
-        const query = `SELECT reviews.id, reviews.tagline, reviews.posting_date, reviews.review_body, ratings.name as rating_name, ratings.stars as stars FROM reviews 
+        const query = `SELECT reviews.id, reviews.tagline, to_char(reviews.posting_date, 'Mon DD, YYYY') as posting_date, reviews.review_body, ratings.name as rating_name, ratings.stars as stars FROM reviews 
         INNER JOIN ratings
         ON reviews.stars_id = ratings.id
         where user_id = '${user_id}'
         ORDER BY reviews.posting_date DESC;`;
         const response = await db.any(query);
         return response;
+    }
+
+    static async getReviewById(id) {
+        const query = `SELECT * FROM reviews WHERE id = '${id}';`;
+        const response = await db.one(query);
+        return response;
+    }
+
+    static async editReview(review_id, tagline, review_body, stars_id) {
+        const query = `UPDATE reviews SET tagline = '${tagline}', review_body = '${review_body}', stars_id = '${stars_id}' WHERE id = '${review_id}';`;
+        try {
+            const response = await db.none(query);
+            return response;
+        } catch (err) {
+            return err;
+        }
+    }
+
+    static async deleteReview(id) {
+        const query = `DELETE FROM reviews WHERE id = '${id}';`;
+        try {
+            const response = await db.none(query);
+            return response;
+        } catch (err) {
+            return err;
+        }
     }
 };
 
--- routes/actors.js
+++ routes/actors.js
@@ -2,11 +2,11 @@
 
 const express = require('express'),
     router = express.Router(),
-    showsModel = require('../models/showModel');
+    actorsModel = require('../models/actorModel');
 
 
 router.get('/', async (req, res) => {
-    const actorsList = await showsModel.getAllActors();
+    const actorsList = await actorsModel.getAllActors();
     res.render('template', {
         locals: {
             title: "TV Show Actors",
@@ -19,6 +19,25 @@ router.get('/', async (req, res) => {
         }
     });
 });
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const actor = await actorsModel.getActorInfoByID(id);
+    const showList = await actorsModel.getShowsByActorID(id);
+    console.log(showList);
+    res.render('template', {
+        locals: {
+            title: "TV Show Actors",
+            is_logged_in: req.session.is_logged_in,
+            showList,
+            actor,
+        },
+        partials: {
+            header: 'partials/header',
+            body: 'partials/actor_shows',
+        }
+    })
+})
 
 
 module.exports = router;
